103aea7e891153eb38b1d9bdd4aeba6512bb7867
Bruno Braga <bbchagas@gmail.com>
2016-07-29 17:07:24 -0300

    Finish Youtube Player APP

--- app/src/main/java/com/example/bruno/youtubeplayer/StandaloneActivity.java
+++ app/src/main/java/com/example/bruno/youtubeplayer/StandaloneActivity.java
@@ -1,21 +1,38 @@
 package com.example.bruno.youtubeplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class StandaloneActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+public class StandaloneActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String GOOGLE_API_KEY = "AIzaSyD3w1DPfWEa0_OSVc9aZ8Se0pYhHiVfYd8";
+    private String YOUTUBE_VIDEO_ID = "1VQ_3sBZEm0";
+    private String YOUTUBE_PLAYLIST = "PLyjqnQsvvnpKG7SC3BqHF08lMWOp8T5Er";
+    private Button btnPlayVideo;
+    private Button btnPlayPlaylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standalone);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        btnPlayVideo = (Button) findViewById(R.id.btnPlayVideo);
+        btnPlayPlaylist = (Button) findViewById(R.id.btnPlaylist);
+
+        btnPlayVideo.setOnClickListener(this);
+        btnPlayPlaylist.setOnClickListener(this);
+
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -25,5 +42,25 @@ public class StandaloneActivity extends AppCompatActivity {
 //            }
 //        });
     }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.btnPlayVideo:
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, GOOGLE_API_KEY,YOUTUBE_VIDEO_ID);
+                break;
+            case R.id.btnPlaylist:
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, YOUTUBE_PLAYLIST);
+                break;
+            default:
+        }
+
+        if(intent != null){
+            startActivity(intent);
+        }
+
+    }
+
 
 }
